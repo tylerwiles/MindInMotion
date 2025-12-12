694c9f80e4b9566d6563b9598b2a2b48600e3c3b
Wiles <twiles@ufl.edu>
2025-12-12 13:29:15 -0500

    HSIM Converted MATLAB to R from exported time series

--- MATLAB/H_Simulation_MIM.m
+++ MATLAB/H_Simulation_MIM.m
@@ -5,10 +5,9 @@
 % EMAIL: twiles@ufl.edu
 
 % DESCRIPTION:
-% Take stride intervals from the mind in motion dataset and estimate the
-% Hurst exponent. The Hurst exponent is calculated for the final 50, 100,
-% 150, 200 strides (when the number of strides allows) and then the time
-% series is dropped from 2-10% randomly or contiguously.
+% Take the stride intervals from the mind in motion dataset and export them
+% as individual files, per trial, to be thrown into the rscript with the
+% same name. This script has to be run at school because of EEGLab.
 
 clear; close all; clc;
 
@@ -18,7 +17,7 @@ addpath(genpath('MATLAB'));
 addpath(genpath('R:\Ferris-Lab\share\MindInMotion\eeglab'));
 eeglab % Load EEGLAB
 my_directory = 'R:\Ferris-Lab\share\MindInMotion\Data';
-output_directory = 'C:\Users\twiles\Desktop\Github-Repositories\MindInMotion\DATA\H_Simulation';
+output_directory = 'R:\Ferris-Lab\twiles\H_Simulation\MIM';
 
 my_folders = dir(fullfile(my_directory, 'H*'));
 my_folders = [my_folders; dir(fullfile(my_directory, 'NH*'))];
@@ -35,30 +34,6 @@ end
 my_files = vertcat(files{:});
 my_files(strcmp({my_files.name}, 'TM_low_2.set') & strcmp({my_files.folder}, 'R:\Ferris-Lab\share\MindInMotion\Data\NH3027\EEG\Trials')) = []; % No data for this trial?
 
-drops = 0.02:0.02:0.10;
-min_vec = [51 101 151 201];
-
-n_files = length(my_files);
-n_drops = numel(drops);
-n_min = numel(min_vec);
-n_rep = 50; % number of repetitions for each file / min_contacts / drop
-
-% one H per file x min_contacts setting
-h_original = NaN(n_files, n_min);
-id = cell(n_files, n_min);
-treadmill = cell(n_files, n_min);
-speed = cell(n_files, n_min);
-terrain = cell(n_files, n_min);
-trial = cell(n_files, n_min);
-
-% per file x min_contacts x %drop x rep
-drops_used = NaN(n_files, n_min, n_drops);
-strides = NaN(n_files, n_min, n_drops);
-strides_cut = NaN(n_files, n_min, n_drops);
-strides_new_length = NaN(n_files, n_min, n_drops);
-h_random = NaN(n_files, n_min, n_drops, n_rep);
-h_contiguous = NaN(n_files, n_min, n_drops, n_rep);
-tic()
 parfor i = 1:length(my_files)
 
     % Getting filenames that will be useful for the rest of the code
@@ -69,6 +44,13 @@ parfor i = 1:length(my_files)
     filename_split = strsplit(base_filename, '_');
     temp_id = strsplit(full_filename, '\');
     temp_id_condition = strsplit(base_filename, '_');
+
+    id = temp_id{6};
+    treadmill = temp_id_condition{1};
+    speed = temp_id_condition{2};
+    terrain = 'flat';
+    trial = temp_id_condition{3}(1);
+    output_name = [id, '_', treadmill, '_', speed, '_', terrain, '_', trial, '.csv'];
 
     dat_temp = pop_loadset(full_filename); % Load in the EEGLab structure
     dat = struct2table(dat_temp.event); % Just get time series of gait events
@@ -82,147 +64,9 @@ parfor i = 1:length(my_files)
     contacts_right = find(strcmp(dat.type, 'RHS'));
     contacts_latency = table2array(dat(contacts_right, 1));
 
-    % Loop over minimum contacts thresholds
-    for k = 1:n_min
+    % Calculate step or stride intervals
+    stride_intervals = diff(contacts_latency) / dat_temp.srate;
 
-        min_contacts = min_vec(k);
-
-        % only run for this threshold if we have enough contacts
-        if numel(contacts_latency) >= min_contacts
-
-            % Cut to last min_contacts
-            contacts_latency_cut = contacts_latency(max(1, numel(contacts_latency) - min_contacts + 1) : end);
-
-            % Calculate step or stride intervals
-            intervals = diff(contacts_latency_cut) / dat_temp.srate;
-
-            % H for original series for this (file, min_contacts) pair
-            h_original(i,k) = median(bayesH(intervals, 200));
-
-            % local row buffers for this file & threshold
-            drops_used_row = NaN(1, n_drops);
-            strides_row = NaN(1, n_drops);
-            strides_cut_row = NaN(1, n_drops);
-            strides_new_length_row = NaN(1, n_drops);
-            h_random_row = NaN(n_drops, n_rep);
-            h_contiguous_row = NaN(n_drops, n_rep);
-            id{i} = temp_id{6};
-            treadmill{i} = temp_id_condition{1};
-            speed{i} = temp_id_condition{2};
-            terrain{i} = 'flat';
-            trial{i} = temp_id_condition{3}(1);
-
-            n_total = length(intervals); % total strides for this file
-
-            % Cut strides based on %
-            for j = 1:numel(drops)
-
-                drops_used_row(j) = drops(j); % Current % of strides to be droppped
-
-                n_keep = n_total - floor(n_total * drops(j)); % How many strides to keep
-
-                strides_row(j) = n_total; % Number of strides
-                strides_cut_row(j) = n_total - n_keep; % Number of strides cut
-
-                strides_new_length_row(j) = n_keep;  % Length of new time series
-
-                % run the random / contiguous removal n_rep times
-                for r = 1:n_rep
-
-                    % random removal
-                    intervals_random = intervals(sort(randperm(n_total, n_keep))); % Cut the dataframe
-                    h_random_row(j,r) = median(bayesH(intervals_random, 200)); % Get H
-
-                    % Do the same for contiguous strides
-                    n_drop = n_total - n_keep; % Number of strides to cut
-
-                    start_idx = randi(n_total - n_drop + 1); % choose random start for contiguous block to cut
-
-                    % Create index of those to keep, then assign those that will be removed a zero
-                    keep_mask = true(n_total, 1);
-                    keep_mask(start_idx:start_idx + n_drop - 1) = false;
-
-                    intervals_contiguous = intervals(keep_mask); % Cut based on index above
-
-                    h_contiguous_row(j,r) = median(bayesH(intervals_contiguous, 200)); % Get H
-
-                end
-
-            end
-
-            % assign rows once per file & threshold (parfor-compatible slicing)
-            strides(i,k,:) = strides_row;
-            drops_used(i,k,:) = drops_used_row;
-            strides_cut(i,k,:) = strides_cut_row;
-            strides_new_length(i,k,:) = strides_new_length_row;
-            h_random(i,k,:,:) = reshape(h_random_row, [1 1 n_drops n_rep]);
-            h_contiguous(i,k,:,:) = reshape(h_contiguous_row, [1 1 n_drops n_rep]);
-
-        end
-
-    end
+    writematrix(stride_intervals, fullfile(output_directory, output_name));
 
 end
-toc()
-% Combine outputs from parfor into one long table
-n_files = length(my_files);
-n_drops = numel(drops);
-n_min = numel(min_vec);
-
-% Flatten per-(file, min, drop, rep) matrices into column vectors
-% First, replicate non-rep quantities across rep dimension
-strides_rep = repmat(strides, 1, 1, 1, n_rep);
-drops_rep = repmat(drops_used, 1, 1, 1, n_rep);
-strides_cut_rep = repmat(strides_cut, 1, 1, 1, n_rep);
-strides_new_length_rep = repmat(strides_new_length, 1, 1, 1, n_rep);
-
-strides_col = strides_rep(:);
-drops_col = drops_rep(:);
-strides_cut_col = strides_cut_rep(:);
-strides_new_length_col = strides_new_length_rep(:);
-
-h_random_col = h_random(:);
-h_contiguous_col = h_contiguous(:);
-
-% Repeat original H for each drop level and rep
-h_original_rep = repmat(h_original, 1, 1, n_drops, n_rep);
-h_original_col = h_original_rep(:);
-
-% Do the same for the id condition variables
-id(:, 2:end) = repmat(id(:,1), 1, size(id,2)-1);
-id_rep = repmat(id, 1, 1, n_drops, n_rep);
-id_col = id_rep(:);
-
-treadmill(:, 2:end) = repmat(treadmill(:,1), 1, size(treadmill,2)-1);
-treadmill_rep = repmat(treadmill, 1, 1, n_drops, n_rep);
-treadmill_col = treadmill_rep(:);
-
-speed(:, 2:end) = repmat(speed(:,1), 1, size(speed,2)-1);
-speed_rep = repmat(speed, 1, 1, n_drops, n_rep);
-speed_col = speed_rep(:);
-
-terrain(:, 2:end) = repmat(terrain(:,1), 1, size(terrain,2)-1);
-terrain_rep = repmat(terrain, 1, 1, n_drops, n_rep);
-terrain_col = terrain_rep(:);
-
-trial(:, 2:end) = repmat(trial(:,1), 1, size(trial,2)-1);
-trial_rep = repmat(trial, 1, 1, n_drops, n_rep);
-trial_col = trial_rep(:);
-
-% repetition index
-rep_mat = repmat(reshape(1:n_rep, 1, 1, 1, n_rep), [n_files, n_min, n_drops, 1]);
-rep_col = rep_mat(:);
-
-% Build table in desired column order
-results = table(id_col, treadmill_col, speed_col, terrain_col, trial_col, rep_col, ...
-    strides_col, drops_col, strides_cut_col, strides_new_length_col, ...
-    h_original_col, h_random_col, h_contiguous_col, ...
-    'VariableNames', {'id', 'treadmill', 'speed', 'terrain', 'trial', 'rep', ...
-    'strides', 'drops', 'strides.cut', 'strides.new.length', ...
-    'h.original', 'h.random', 'h.contiguous'});
-
-% % Remove skipped trials / thresholds
-% valid_idx = ~isnan(h_random_col);
-% results = results(valid_idx,:);
-
-writetable(results, fullfile(output_directory, 'H_Simulation_MIM_Results.csv'));
---
+++ R/H_Simulation_Gaitprint.R
@@ -0,0 +1,180 @@
+
+# Description: Before this script is run, the matlab script with the same name
+# should have already be run. From the time series of stride intervals from the
+# Gaitprint dataset, the Hurst exponent is calculated for the final 50, 100,
+# 150, 200 strides (when the number of strides allows) and then the time series
+# is dropped from 2-10% randomly or contiguously. Each of these combinations of
+# time series lengths and % drops are run multiple times (i.e., 100 repetitions)
+# but the average of them are exported. This code is written to be run on
+# Hipergator but minor changes will run locally.
+
+options(scipen = 999)
+rm(list = ls())
+
+library(data.table)
+library(doParallel)
+library(dplyr)
+library(here)
+library(foreach)
+library(parallel)
+library(Rcpp)
+library(RcppArmadillo)
+
+my.directory = "/blue/dferris/twiles/HSIM_DATA/H_Simulation/GAITPRINT"
+# my.directory = "R:/Ferris-Lab/twiles/H_Simulation/GAITPRINT" For Testing
+my.files = list.files(my.directory, full.names = FALSE)
+
+drops = seq(0.02, 0.10, by = 0.02)
+min.vec = c(50, 100, 150, 200)
+
+n.files = length(my.files)
+n.drops = length(drops)
+n.min = length(min.vec)
+n.rep = 100 # number of repetitions for each i / min.contacts / drop
+
+# Parallel Setup --------------------------------------------------------------------
+
+# Rcpp::sourceCpp(here::here('FUNCTIONS', 'CPP', 'bayesH.cpp')) # For Testing
+cpp.path = here::here('FUNCTIONS', 'CPP', 'bayesH.cpp')
+
+slurm.cpus = as.integer(Sys.getenv("SLURM_CPUS_PER_TASK", ""))
+if (is.na(slurm.cpus) || slurm.cpus < 1L) {
+  # physical cores preferred (avoid hyperthreads)
+  n.cores = max(1L, parallel::detectCores(logical = FALSE))
+} else {
+  n.cores = slurm.cpus
+}
+print('done 1')
+cl = makeCluster(n.cores)
+print('done 2')
+registerDoParallel(cl)
+print('done 3')
+
+clusterExport(cl, varlist = c("cpp.path"), envir = environment()) # export the C++ file path to workers
+print('done 4')
+
+# sync library paths
+lib.paths = .libPaths()
+clusterCall(cl, function(p) .libPaths(p), lib.paths)
+print('done 5')
+
+clusterEvalQ(cl, {
+  suppressPackageStartupMessages({
+    library(Rcpp)
+    library(RcppArmadillo)
+  })
+  Rcpp::sourceCpp(cpp.path, rebuild = FALSE, showOutput = FALSE, verbose = FALSE)
+  TRUE
+})
+print('done 6')
+
+# Run Gaitprint Data ------------------------------------------------------
+
+result.lists = foreach(i = seq_len(n.files),
+                       .export = c("drops", "min.vec", "n.drops", "n.min", "n.rep",
+                                   "my.directory", "my.files"),
+                       .packages = c("data.table"),
+                       .combine = "c") %dopar% {
+                         
+                         base.filename = my.files[i]
+                         full.filename = file.path(my.directory, base.filename)
+                         
+                         png.filename = strsplit(base.filename, ".", fixed = TRUE)[[1]][1]
+                         
+                         cat("Analyzing:", png.filename)
+                         
+                         dat = data.table::fread(full.filename, header = FALSE)
+                         dat = as.numeric(dat[[1L]])
+                         
+                         # Max possible rows for this file: one row per (min.contacts, drop)
+                         max.rows.i = n.min * n.drops
+                         rows.list.i = vector("list", length = max.rows.i)
+                         row.idx.i = 0L
+                         
+                         for (k in seq_len(n.min)) { # Loop over minimum number of stride intervals
+                           
+                           min.contacts = min.vec[k]
+                           
+                           if (length(dat) >= min.contacts) { # only run for this threshold if we have enough contacts
+                             
+                             dat.cut = tail(dat, min.contacts) # Cut to last min.contacts
+                             
+                             h.orig.val = median(bayesH(dat.cut, 200)) # H for original series for this (file, min.contacts) pair
+                             
+                             n.total = length(dat.cut) # total strides for this file
+                             
+                             for (j in seq_len(n.drops)) { # Loop over % drops
+                               
+                               this.drop = drops[j]
+                               
+                               n.keep = n.total - floor(n.total * this.drop)
+                               n.drop = n.total - n.keep
+                               
+                               strides.val = n.total
+                               strides.cut.val = n.drop
+                               strides.new.length.val = n.keep
+                               
+                               # collect H values over n.rep repetitions
+                               h.random.vec = numeric(n.rep)
+                               h.contig.vec = numeric(n.rep)
+                               
+                               for (r in seq_len(n.rep)) { # Loop over number of repetitions
+                                 
+                                 # random removal
+                                 keep.idx.random = sort(sample.int(n.total, n.keep))
+                                 dat.cut.random = dat.cut[keep.idx.random]
+                                 h.random.vec[r] = median(bayesH(dat.cut.random, 200))
+                                 
+                                 # contiguous removal
+                                 start.idx = sample.int(n.total - n.drop + 1L, 1L)
+                                 
+                                 keep.mask = rep(TRUE, n.total)
+                                 if (n.drop > 0L) {
+                                   keep.mask[start.idx:(start.idx + n.drop - 1L)] = FALSE
+                                 }
+                                 
+                                 dat.cut.contiguous  = dat.cut[keep.mask]
+                                 h.contig.vec[r] = median(bayesH(dat.cut.contiguous, 200))
+                               }
+                               
+                               # Average across reps
+                               h.random.mean = mean(h.random.vec)
+                               h.contig.mean = mean(h.contig.vec)
+                               
+                               row.idx.i = row.idx.i + 1L # Store ONE row per (file, min.contacts, drop)
+                               
+                               rows.list.i[[row.idx.i]] = list(id = png.filename,
+                                                               rep = n.rep, # Number of repetitions that were averaged
+                                                               stride.intervals = strides.val, # Number of original strides
+                                                               drops = this.drop, # % of trial to be dropped
+                                                               stride.intervals.cut = strides.cut.val, # Number of strides cut
+                                                               stride.intervals.new.length = strides.new.length.val, # New number of strides
+                                                               h.original = h.orig.val, # H from the original time series
+                                                               h.random = h.random.mean, # H from the randomly cut timeseries
+                                                               h.contiguous = h.contig.mean # H from the contiguously cut timeseries
+                               )
+                               
+                             }
+                             
+                           }
+                           
+                         }
+                         
+                         # Trim unused entries for this file
+                         if (row.idx.i < length(rows.list.i)) {
+                           rows.list.i = rows.list.i[seq_len(row.idx.i)]
+                         }
+                         
+                         rows.list.i
+                       }
+
+stopCluster(cl)
+
+# Flatten and make table
+rows.list.all = result.lists
+results = do.call(rbind.data.frame, c(rows.list.all, list(stringsAsFactors = FALSE)))
+results = results[, c("id", "rep", "stride.intervals", "drops",
+                      "stride.intervals.cut", "stride.intervals.new.length",
+                      "h.original", "h.random", "h.contiguous")]
+
+write.csv(results, here::here("DATA", "H_Simulation", "H_Simulation_Gaitprint.csv"), row.names = FALSE)
