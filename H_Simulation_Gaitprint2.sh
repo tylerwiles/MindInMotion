8b7b7a9cf3cb1ff2afbf70715fe9a9bc3eef3e71
Wiles <twiles@ufl.edu>
2025-12-15 15:06:48 -0500

    New Gaitprint Results

---
+++ H_Simulation_Gaitprint2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Required Items
+#SBATCH --nodes=1 # Number of Physical Servers
+#SBATCH --ntasks=1 # Number of CPUs
+#SBATCH --cpus-per-task=24 # Number of cores per task
+#SBATCH --mem=768gb # Job memory request
+#SBATCH --distribution=cyclic:cyclic # Distribute tasks cyclically 
+#SBATCH --time=0-12:00 # Time limit days-hrs:min
+#SBATCH --qos=dferris-b # -b to use Burst
+
+# Optional Items
+#SBATCH --job-name=HSMGP2 # Job name 
+#SBATCH --mail-type=END,FAIL # Mail events 
+#SBATCH --mail-user=twiles@ufl.edu # Where to send mail 
+#SBATCH --output=HSMGP2%j.log # Standard output and error log 
+pwd; hostname; date # Some user information
+
+# Run the code
+module purge
+module load R # Load needed modules
+
+# Important for packages
+export R_LIBS_USER="/blue/dferris/twiles/Rlibs"
+mkdir -p "$R_LIBS_USER"
+echo "Using personal R library at: $R_LIBS_USER"
+
+echo "Running H Simulation Test" # Tell me its doing something
+
+Rscript /blue/dferris/twiles/MindInMotion/R/H_Simulation_Gaitprint2.R
+
+date # Print ending time
--- H_Simulation_Gaitprint3.sh
+++ H_Simulation_Gaitprint3.sh
@@ -4,16 +4,16 @@
 #SBATCH --nodes=1 # Number of Physical Servers
 #SBATCH --ntasks=1 # Number of CPUs
 #SBATCH --cpus-per-task=24 # Number of cores per task
-#SBATCH --mem=256gb # Job memory request
+#SBATCH --mem=384gb # Job memory request
 #SBATCH --distribution=cyclic:cyclic # Distribute tasks cyclically 
-#SBATCH --time=0-06:00 # Time limit days-hrs:min
-#SBATCH --qos=dferris # -b to use Burst
+#SBATCH --time=0-9:00 # Time limit days-hrs:min
+#SBATCH --qos=dferris-b # -b to use Burst
 
 # Optional Items
-#SBATCH --job-name=HSIM_Gaitprint # Job name 
+#SBATCH --job-name=HSMGP3 # Job name 
 #SBATCH --mail-type=END,FAIL # Mail events 
 #SBATCH --mail-user=twiles@ufl.edu # Where to send mail 
-#SBATCH --output=HSIM_Gaitprint%j.log # Standard output and error log 
+#SBATCH --output=HSMGP3%j.log # Standard output and error log 
 pwd; hostname; date # Some user information
 
 # Run the code
@@ -27,6 +27,6 @@ echo "Using personal R library at: $R_LIBS_USER"
 
 echo "Running H Simulation Test" # Tell me its doing something
 
-Rscript /blue/dferris/twiles/MindInMotion/R/H_Simulation_Gaitprint.R
+Rscript /blue/dferris/twiles/MindInMotion/R/H_Simulation_Gaitprint3.R
 
 date # Print ending time
